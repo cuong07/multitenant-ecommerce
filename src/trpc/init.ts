6f0f75579d21bce11463a2f6ffb290487bcb1da5
Tommy Shelby <front.cuong@gmail.com>
2025-08-17 14:04:15 +0700

    fix: Enable superjson for tRPC data transformation

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,7 +1,8 @@
+import configPromise from "@payload-config";
 import { initTRPC } from "@trpc/server";
 import { getPayload } from "payload";
 import { cache } from "react";
-import configPromise from "@payload-config";
+import superjson from "superjson";
 
 export const createTRPCContext = cache(async () => {
   /**
@@ -17,7 +18,7 @@ const t = initTRPC.create({
   /**
    * @see https://trpc.io/docs/server/data-transformers
    */
-  // transformer: superjson,
+  transformer: superjson,
 });
 // Base router and procedure helpers
 export const createTRPCRouter = t.router;
